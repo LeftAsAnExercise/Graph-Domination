46a11ef9200d93f51e0f42e98c0c64e1d87cd3e9
LeftAsAnExercise <76869775+LeftAsAnExercise@users.noreply.github.com>
2021-01-30 17:30:33 -0600

    Add files via upload

--- DominationGraphMaker.py
+++ DominationGraphMaker.py
@@ -5,26 +5,52 @@ def latticemaker(n):
             latticelist.append([x,y])
 
     return latticelist
-def moveset():
-    return [[2,1],[2,-1],[-2,-1],[-2,1],[1,2],[1,-2],[-1,-2],[-1,2]]
+def moveset(r,s):
+    return [[r,s],[r,-s],[-r,-s],[-r,s],[s,r],[s,-r],[-s,-r],[-s,r]]
 def vectoradd(n,k):
-    
-    for i in range(len(n)):
-      n[i]+=k[i] 
+    placeholder=n
+    for i in range(len(placeholder)):
+      placeholder[i]+=k[i] 
        
-    return n
+    return placeholder
 
 def horsegraph(n):
     #this isn't done
     lattice=latticemaker(n)
-    movesets=moveset()
-    for n in lattice:
+    print(lattice)
+    print("__________")
+    movesets=moveset(1,2)
+    for m in lattice:
+        print(m)
+        print("above is m")
         for k in movesets:
-            Ll=vectoradd(n,k)
-            if Ll in lattice:
-                print(vectoradd(n,k))
+            Ll=vectoradd(m,k)
+            if (Ll[0]>=0 and Ll[0]<=n-1) and (Ll[1]>=0 and Ll[1]<=n-1):
+                print(Ll)
+                
                 continue
             else:
                 continue
     return 
-print(latticemaker(5))
+
+
+    
+def rsLeaperGraph(n,r,s):
+    lattice=latticemaker(n)
+    banana=moveset(r,s)
+    print(lattice)
+    for x in range(n):
+        for y in range(n):
+            
+            for k in banana:
+                if vectoradd([x,y],k) in lattice:
+                    print("_______")
+                    print(vectoradd([x,y],k))
+                    print("yes")
+                    continue
+                else:
+                    continue
+    return
+
+            
+rsLeaperGraph(40,1,2)
