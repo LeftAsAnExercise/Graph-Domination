2e206a86b37237c78abcedca35b925807eed1c7e
AB3000 <aarushban@gmail.com>
2021-01-30 16:12:08 -0800

    removed extra comments

--- graphtest.py
+++ graphtest.py
@@ -1,39 +1,3 @@
-# First networkx library is imported  
-# # along with matplotlib 
-# import networkx as nx 
-# import matplotlib.pyplot as plt 
-   
-  
-# # Defining a Class 
-# class GraphVisualization: 
-   
-#     def __init__(self): 
-          
-#         # visual is a list which stores all  
-#         # the set of edges that constitutes a 
-#         # graph 
-#         self.visual = [] 
-          
-#     # addEdge function inputs the vertices of an 
-#     # edge and appends it to the visual list 
-#     def addEdge(self, a, b): 
-#         temp = [a, b] 
-#         self.visual.append(temp) 
-          
-#     # In visualize function G is an object of 
-#     # class Graph given by networkx G.add_edges_from(visual) 
-#     # creates a graph with a given list 
-#     # nx.draw_networkx(G) - plots the graph 
-#     # plt.show() - displays the graph 
-#     def visualize(self): 
-#         G = nx.Graph() 
-#         G.add_edges_from(self.visual) 
-#         nx.draw_networkx(G) 
-#         plt.show() 
-  
-# # Driver code
-
-
 
 def generateRooksGraph(n):
     graph = dict()
@@ -51,16 +15,8 @@ def generateRooksGraph(n):
 
 
 if __name__ == "__main__":
-    # n = 20
-    # size = n
     graph = generateRooksGraph(8) # 1 - 8
     print(graph)
-    # vertexes = graph.keys()
-    # G = GraphVisualization() 
-    # for i in vertexes:
-    #     for j in graph[i]:
-    #         G.addEdge(i,j)
 
-    # G.visualize() 
 
 
