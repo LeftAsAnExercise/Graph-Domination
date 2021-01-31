424bc090e87eb7203e44cb83ddaf01c353d141ec
AB3000 <aarushban@gmail.com>
2021-01-30 16:03:59 -0800

    Created movesets for rooks

--- graphtest.py
+++ graphtest.py
@@ -1,70 +1,77 @@
 # First networkx library is imported  
-# along with matplotlib 
-import networkx as nx 
-import matplotlib.pyplot as plt 
+# # along with matplotlib 
+# import networkx as nx 
+# import matplotlib.pyplot as plt 
    
   
-# Defining a Class 
-class GraphVisualization: 
+# # Defining a Class 
+# class GraphVisualization: 
    
-    def __init__(self): 
+#     def __init__(self): 
           
-        # visual is a list which stores all  
-        # the set of edges that constitutes a 
-        # graph 
-        self.visual = [] 
+#         # visual is a list which stores all  
+#         # the set of edges that constitutes a 
+#         # graph 
+#         self.visual = [] 
           
-    # addEdge function inputs the vertices of an 
-    # edge and appends it to the visual list 
-    def addEdge(self, a, b): 
-        temp = [a, b] 
-        self.visual.append(temp) 
+#     # addEdge function inputs the vertices of an 
+#     # edge and appends it to the visual list 
+#     def addEdge(self, a, b): 
+#         temp = [a, b] 
+#         self.visual.append(temp) 
           
-    # In visualize function G is an object of 
-    # class Graph given by networkx G.add_edges_from(visual) 
-    # creates a graph with a given list 
-    # nx.draw_networkx(G) - plots the graph 
-    # plt.show() - displays the graph 
-    def visualize(self): 
-        G = nx.Graph() 
-        G.add_edges_from(self.visual) 
-        nx.draw_networkx(G) 
-        plt.show() 
+#     # In visualize function G is an object of 
+#     # class Graph given by networkx G.add_edges_from(visual) 
+#     # creates a graph with a given list 
+#     # nx.draw_networkx(G) - plots the graph 
+#     # plt.show() - displays the graph 
+#     def visualize(self): 
+#         G = nx.Graph() 
+#         G.add_edges_from(self.visual) 
+#         nx.draw_networkx(G) 
+#         plt.show() 
   
-# Driver code
+# # Driver code
 
 
 
-def generateBishopsGraph(n):
+def generateRooksGraph(n):
     graph = dict()
-    for i in range(n):
-        for j in range(1,n+1):
+    for i in range(n): # horizontal (letters / other ASCII characters)
+        for j in range(1,n+1): #vertical (numbers)
             s = chr(i+97) + str(j)
             graph[s] = []
-            for k in range(-n,n):
-                index = j + k
-                letter = i + k
-                if not (letter < 0 or letter > n-1) and not (index <= 0 or index > n) and not (k == 0):
-                    v = chr(letter+97) + str(index)
-                    graph[s].append(v)
-                index = j - k
-                letter = i + k
-                if not (letter < 0 or letter > n-1) and not (index <= 0 or index > n) and not (k == 0):
-                    v = chr(letter+97) + str(index)
-                    graph[s].append(v)
+
+            moves_across = [chr(i+97) + str(k + 1) for k in range(n)]
+            moves_vertical = [chr(k+97) + str(j) for k in range(n)]
+
+            graph[s].append(moves_across + moves_vertical)
+
+            # for k in range(-n,n):
+            #     index = j + k
+            #     letter = i + k
+            #     if not (letter < 0 or letter > n-1) and not (index <= 0 or index > n) and not (k == 0):
+            #         v = chr(letter+97) + str(index)
+            #         graph[s].append(v)
+            #     index = j - k
+            #     letter = i + k
+            #     if not (letter < 0 or letter > n-1) and not (index <= 0 or index > n) and not (k == 0):
+            #         v = chr(letter+97) + str(index)
+            #         graph[s].append(v)
     return graph
 
 
 if __name__ == "__main__":
-    n = 20
-    size = n
-    graph = generateBishopsGraph(n)
-    vertexes = graph.keys()
-    G = GraphVisualization() 
-    for i in vertexes:
-        for j in graph[i]:
-            G.addEdge(i,j)
+    # n = 20
+    # size = n
+    graph = generateRooksGraph(8) # 1 - 8
+    print(graph)
+    # vertexes = graph.keys()
+    # G = GraphVisualization() 
+    # for i in vertexes:
+    #     for j in graph[i]:
+    #         G.addEdge(i,j)
 
-    G.visualize() 
+    # G.visualize() 
 
 
