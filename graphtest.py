f7b1167e202fc86720ca0cc51ba805e6a1d6d274
bryYak <byakimis@purdue.edu>
2021-01-30 19:00:01 -0500

    Added Kings Graph

--- graphtest.py
+++ graphtest.py
@@ -30,8 +30,6 @@ class GraphVisualization:
         G.add_edges_from(self.visual) 
         nx.draw_networkx(G) 
         plt.show() 
-  
-# Driver code
 
 
 
@@ -39,26 +37,42 @@ def generateBishopsGraph(n):
     graph = dict()
     for i in range(n):
         for j in range(1,n+1):
-            s = chr(i+97) + str(j)
-            graph[s] = []
+            currentSquare = chr(i+97) + str(j) # a1
+            graph[currentSquare] = []
             for k in range(-n,n):
                 index = j + k
                 letter = i + k
                 if not (letter < 0 or letter > n-1) and not (index <= 0 or index > n) and not (k == 0):
-                    v = chr(letter+97) + str(index)
-                    graph[s].append(v)
+                    validMove = chr(letter+97) + str(index)
+                    graph[currentSquare].append(validMove)
                 index = j - k
                 letter = i + k
                 if not (letter < 0 or letter > n-1) and not (index <= 0 or index > n) and not (k == 0):
-                    v = chr(letter+97) + str(index)
-                    graph[s].append(v)
+                    validMove = chr(letter+97) + str(index)
+                    graph[currentSquare].append(validMove)
+    return graph
+#Maybe add currentsquare into list
+def generateKingsGraph(n):
+    graph = dict()
+    for i in range(n):
+        for j in range(1,n+1):
+            currentSquare = chr(i+97) + str(j)
+            graph[currentSquare] = []
+            for k in range(-1,2):
+                for l in range(-1,2):
+                    letter = i + k
+                    index = j + l
+                    if not (letter < 0 or letter > n-1) and not (index <= 0 or index > n) and not (k==0 and l==0):
+                        validMove = chr(letter+97) + str(index)
+                        graph[currentSquare].append(validMove)
     return graph
 
 
 if __name__ == "__main__":
-    n = 20
+    n = 3
     size = n
-    graph = generateBishopsGraph(n)
+    graph = generateKingsGraph(n)
+    print(graph)
     vertexes = graph.keys()
     G = GraphVisualization() 
     for i in vertexes:
