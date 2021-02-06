e2285ed2237bf53d1d2c5d65a229032f778650fc
bryYak <70532532+bryYak@users.noreply.github.com>
2021-02-06 18:12:15 -0500

    Update graphtest.py

--- graphtest.py
+++ graphtest.py
@@ -1,38 +1,3 @@
-# First networkx library is imported  
-# along with matplotlib 
-import networkx as nx 
-import matplotlib.pyplot as plt 
-   
-  
-# Defining a Class 
-class GraphVisualization: 
-   
-    def __init__(self): 
-          
-        # visual is a list which stores all  
-        # the set of edges that constitutes a 
-        # graph 
-        self.visual = [] 
-          
-    # addEdge function inputs the vertices of an 
-    # edge and appends it to the visual list 
-    def addEdge(self, a, b): 
-        temp = [a, b] 
-        self.visual.append(temp) 
-          
-    # In visualize function G is an object of 
-    # class Graph given by networkx G.add_edges_from(visual) 
-    # creates a graph with a given list 
-    # nx.draw_networkx(G) - plots the graph 
-    # plt.show() - displays the graph 
-    def visualize(self): 
-        G = nx.Graph() 
-        G.add_edges_from(self.visual) 
-        nx.draw_networkx(G) 
-        plt.show() 
-
-
-
 def generateBishopsGraph(n):
     graph = dict()
     for i in range(n):
@@ -69,16 +34,8 @@ def generateKingsGraph(n):
 
 
 if __name__ == "__main__":
-    n = 3
-    size = n
+    size = 3
     graph = generateKingsGraph(n)
     print(graph)
-    vertexes = graph.keys()
-    G = GraphVisualization() 
-    for i in vertexes:
-        for j in graph[i]:
-            G.addEdge(i,j)
-
-    G.visualize() 
 
 
