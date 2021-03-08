e6e1135eb1cee89d3a33235fb862137cf6f80b07
LeftAsAnExercise <76869775+LeftAsAnExercise@users.noreply.github.com>
2021-03-07 18:53:18 -0600

    stuff

--- src/module.py
+++ src/module.py
@@ -1,15 +1,59 @@
 ##Comment
 from random import choice, uniform
 from numpy import array, where
+def calculateObjectiveFunction(length, graphDictionary, solutionVector):
+    	d = dict()
+	for i in range(len(solutionVector)):
+		if solutionVector[i]:
+			d[i] = graphDictionary[i]
+	V = length ** 2
+	n = getNodes(d)
+	G = solutionVector.count(1)
+	return n/V + 1/(V*G)
 
-class GraphTools:
-   def 
-class SimulatedAnnealing:
-   def NodeSwap(SolutionVector):
-    	   
-    	   return
+def stochastic_local_search(solutionVector):
+    print(calculateObjectiveFunction(solutionVector))
+    while True:
+        objective_function = calculateObjectiveFunction(solutionVector)
+        if(objective_function >= 1):
+            solutionVector[inverse_probability(solutionVector)] = 0
+        else:
+            solutionVector[proportional_probability(solutionVector)] = 1
+        if calculateObjectiveFunction(solutionVector) < objective_function:
+            i1 = int(choice(array(where(array(solutionVector) == 0)).T))
+            i2 = int(choice(array(where(array(solutionVector) == 1)).T))
+            solutionVector[i1], solutionVector[i2] = solutionVector[i2], solutionVector[i1]
+        if(calculateObjectiveFunction(solutionVector) <= objective_function):
+            break
+    return solutionVector       
+    
+  
+def proportional_probability(solutionVector):
+    l = []
+    for index, value in enumerate(solutionVector):
+        if not value:
+            l.extend([index] * len(graphDictionary[index]))
+    return choice(l)
+	
+def inverse_probability(solutionVector):
+    d = {}
+    s = 0
+    for index, value in enumerate(solutionVector):
+        if value:
+            s += 1/len(graphDictionary[index])
+            d[s] = index
+    p = uniform(0,s)
+    for i in d.keys():
+        if p <= i:
+            return d[i]
+	
+def NodeAdd(SolutionVector):
+    solutionVector[proportional_probability(solutionVector)] = 1
 
-	def getNodes(d):
+    return
+	
+def getNodes(d):
+    
 		nodes = []
 		for i in d.keys():
 			if i not in nodes:
@@ -19,13 +63,9 @@ class SimulatedAnnealing:
 					nodes.append(j)
 
 		return len(nodes)
-
-def calculateObjectiveFunction(length, graphDictionary, solutionVector):
-	d = dict()
-	for i in range(len(solutionVector)):
-		if solutionVector[i]:
-			d[i] = graphDictionary[i]
-	V = length ** 2
-	n = getNodes(d)
-	G = solutionVector.count(1)
-	return n/V + 1/(V*G)
+def MainLoop(initTemp, finalTemp, CoolingRatio, EpochLength, initSolutionVector):
+	XBest = initSolutionVector
+	k = 0
+	for M in range(1,EpochLength+1):
+		
+	return 
