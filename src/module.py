efb590602e13cf3f2413ec37ccedbe9262afdcbd
tanaka-spec <yuto.gm@gmail.com>
2021-03-08 09:42:36 +0900

    added func

--- src/module.py
+++ src/module.py
@@ -1,9 +1,50 @@
-##Comment
 class GraphTools:
-   def
-class SimulatedAnnealing:
-   def 
+	def createMovementDictionary(length, func=GraphTools.knight):
+		graphDictionary = []
+		for i in range(len(solutionVector)):
+			graphDictionary = func(i, length)
+		return graphDictionary
 
+	def knight(i, length):
+		output = []
+		x = i % length
+		y = i // length
+		if x - 1 >= 0 and y - 2 >= 0:
+			output.append(x + y * length)
+		if x - 1 >= 0 and y + 2 < length:
+			output.append(x + y * length)
+		if x - 2 >= 0 and y - 1 >= 0:
+			output.append(x + y * length)
+		if x - 2 >= 0 and y + 1 < length:
+			output.append(x + y * length)
+		if x + 2 < length and y - 1 >= 0:
+			output.append(x + y * length)
+		if x + 2 < length and y + 1 < length:
+			output.append(x + y * length)
+		if x + 1 < length and y - 2 >= 0:
+			output.append(x + y * length)
+		if x + 1 < length and y + 2 >= 0:
+			output.append(x + y * length)
+		return output
+
+	def bishop(i, length):
+		output = []
+		x = i % length
+		y = i // length
+		for j in range(length):
+			if x + j < length and y + j < length:
+				output.append((j + x) + (y + j) * length)
+			if x + j < length and y - j >= 0:
+				output.append((j + x) + (y - j) * length)
+			if x - j >= - and y - j >= 0:
+				output.append((j - x) + (y - j) * length)
+			if x - j >= - and y + j < length:
+				output.append((j - x) + (y + j) * length)
+		return output
+			
+
+
+class SimulatedAnnealing:
 	def getNodes(d):
 		nodes = []
 		for i in d.keys():
@@ -15,12 +56,12 @@ class SimulatedAnnealing:
 
 		return len(nodes)
 
-def calculateObjectiveFunction(length, graphDictionary, solutionVector):
-	d = dict()
-	for i in range(len(solutionVector)):
-		if solutionVector[i]:
-			d[i] = graphDictionary[i]
-	V = length ** 2
-	n = getNodes(d)
-	G = solutionVector.count(1)
-	return n/V + 1/(V*G)
+	def calculateObjectiveFunction(length, graphDictionary, solutionVector):
+		d = dict()
+		for i in range(len(solutionVector)):
+			if solutionVector[i]:
+				d[i] = graphDictionary[i]
+		V = length ** 2
+		n = getNodes(d)
+		G = solutionVector.count(1)
+		return n/V + 1/(V*G)
