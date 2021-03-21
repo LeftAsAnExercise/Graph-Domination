9872baf12ccadee8efa15a56612e83537e252bb9
tanaka-spec <yuto.gm@gmail.com>
2021-03-22 08:24:21 +0900

    changed values

--- src/DominationGraphMaker.py
+++ src/DominationGraphMaker.py
@@ -1,56 +1,56 @@
 def latticemaker(n):
-    latticelist=[]
-    for x in range(n):
-        for y in range(n):
-            latticelist.append([x,y])
+	latticelist=[]
+	for x in range(n):
+		for y in range(n):
+			latticelist.append([x,y])
 
-    return latticelist
+	return latticelist
 def moveset(r,s):
-    return [[r,s],[r,-s],[-r,-s],[-r,s],[s,r],[s,-r],[-s,-r],[-s,r]]
+	return [[r,s],[r,-s],[-r,-s],[-r,s],[s,r],[s,-r],[-s,-r],[-s,r]]
 def vectoradd(n,k):
-    placeholder=n
-    for i in range(len(placeholder)):
-      placeholder[i]+=k[i] 
-       
-    return placeholder
+	placeholder=n
+	for i in range(len(placeholder)):
+	  placeholder[i]+=k[i] 
+	   
+	return placeholder
 
 def horsegraph(n):
-    #this isn't done
-    lattice=latticemaker(n)
-    print(lattice)
-    print("__________")
-    movesets=moveset(1,2)
-    for m in lattice:
-        print(m)
-        print("above is m")
-        for k in movesets:
-            Ll=vectoradd(m,k)
-            if (Ll[0]>=0 and Ll[0]<=n-1) and (Ll[1]>=0 and Ll[1]<=n-1):
-                print(Ll)
-                
-                continue
-            else:
-                continue
-    return 
+	#this isn't done
+	lattice=latticemaker(n)
+	print(lattice)
+	print("__________")
+	movesets=moveset(1,2)
+	for m in lattice:
+		print(m)
+		print("above is m")
+		for k in movesets:
+			Ll=vectoradd(m,k)
+			if (Ll[0]>=0 and Ll[0]<=n-1) and (Ll[1]>=0 and Ll[1]<=n-1):
+				print(Ll)
+				
+				continue
+			else:
+				continue
+	return 
 
 
-    
+	
 def rsLeaperGraph(n,r,s):
-    lattice=latticemaker(n)
-    banana=moveset(r,s)
-    print(lattice)
-    for x in range(n):
-        for y in range(n):
-            
-            for k in banana:
-                if vectoradd([x,y],k) in lattice:
-                    print("_______")
-                    print(vectoradd([x,y],k))
-                    print("yes")
-                    continue
-                else:
-                    continue
-    return
+	lattice=latticemaker(n)
+	banana=moveset(r,s)
+	print(lattice)
+	for x in range(n):
+		for y in range(n):
+			
+			for k in banana:
+				if vectoradd([x,y],k) in lattice:
+					print("_______")
+					print(vectoradd([x,y],k))
+					print("yes")
+					continue
+				else:
+					continue
+	return
 
-            
+			
 rsLeaperGraph(40,1,2)
--- src/module.py
+++ src/module.py
@@ -68,8 +68,96 @@ def getNodes(d):
                 nodes.append(j)
 
     return len(nodes)
-    
-		
+
+def knight(i, length):
+	output = []
+	x = i % length
+	y = i // length
+	if x - 1 >= 0 and y - 2 >= 0:
+		output.append([x - 1, y - 2])
+	if x - 1 >= 0 and y + 2 < length:
+		output.append([x - 1, y + 2])
+	if x - 2 >= 0 and y - 1 >= 0:
+		output.append([x - 2, y - 1])
+	if x - 2 >= 0 and y + 1 < length:
+		output.append([x - 2, y + 1])
+	if x + 2 < length and y - 1 >= 0:
+		output.append([x + 2, y +- 1])
+	if x + 2 < length and y + 1 < length:
+		output.append([x + 2, y + 1])
+	if x + 1 < length and y - 2 >= 0:
+		output.append([x + 1, y - 2])
+	if x + 1 < length and y + 2 >= 0:
+		output.append([x + 1, y + 2])
+	return output
+
+def bishop(i, length):
+	output = []
+	x = i % length
+	y = i // length
+	for j in range(length):
+		if x + j < length and y + j < length:
+			output.append([(j + x), (y + j)])
+		if x + j < length and y - j >= 0:
+			output.append([(j + x), (y - j)])
+		if x - j >= - and y - j >= 0:
+			output.append([(j - x),(y - j)])
+		if x - j >= - and y + j < length:
+			output.append([(j - x), (y + j)])
+	return output
+
+def rook(i, length):
+	output = []
+	x = i % length
+	y = i // length
+	for j in range(length):
+		if x + j < length:
+			output.append([(j + x), (y)])
+		if x - j >= 0:
+			output.append([(x - j), (y)])
+		if y + j < length:
+			output.append([(x),(y + j)])
+		if y - j >= 0:
+			output.append([(x), (y - j)])
+	return output
+
+def orthogonal(i, length, limit=1):
+	x = i % length
+	y = i // length
+	for j in range(limit):
+		if x + j < length:
+			output.append([(j + x), (y)])
+		if x - j >= 0:
+			output.append([(x - j), (y)])
+		if y + j < length:
+			output.append([(x),(y + j)])
+		if y - j >= 0:
+			output.append([(x), (y - j)])
+	return output
+
+def queen(i, length):
+	output = []
+	output1 = bishop(i, length)
+	output2 = rook(i, length)
+	return list(set(output1 + output2))
+
+
+def createFairNotation(string):
+	def fair(i, length):
+		output = []
+		if string.contains("nX"):
+			output += bishop(i, length)
+		if string.contains("X")
+			output += orthogonal(i, length, n=n)
+		return list(set(output))
+	return fair
+
+def createMovementDictionary(length, func=knight):
+	graphDictionary = []
+	for i in range(len(solutionVector)):
+		graphDictionary = func(i, length)
+	return graphDictionary
+
 def MainLoop(initTemp, finalTemp, CoolingRatio, EpochLength, initSolutionVector):
 	XBest = initSolutionVector
 	k = 0
