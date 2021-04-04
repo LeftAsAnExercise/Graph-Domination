8df0aab238d25d23fc731cff8f63914dc3105009
LeftAsAnExercise <76869775+LeftAsAnExercise@users.noreply.github.com>
2021-04-04 18:07:43 -0500

    Update module.py

--- src/module.py
+++ src/module.py
@@ -4,15 +4,20 @@ import random
 import numpy
 import math
 
+
+graphDictionary = {0: [5, 7], 1: [8, 6], 2: [3, 7], 3: [8, 2], 4: [], 5: [6, 0], 6: [5, 1], 7: [0, 2], 8: [3, 1]}
+print("banana")
+
 def calculateObjectiveFunction(length, graphDictionary, solutionVector):
     d = dict()
-	for i in range(len(solutionVector)):
-		if solutionVector[i]:
-			d[i] = graphDictionary[i]
-	V = length ** 2
-	n = getNodes(d)
-	G = solutionVector.count(1)
-	return n/V + 1/(V*G)
+    for i in range(len(solutionVector)):
+        if solutionVector[i]:
+            d[i] = graphDictionary[i]
+    V = length ** 2
+    n = getNodes(d)
+    G = solutionVector.count(1)
+    return n/V + 1/(V*G)
+
 
 def stochastic_local_search(solutionVector):
     print(calculateObjectiveFunction(solutionVector))
@@ -28,16 +33,21 @@ def stochastic_local_search(solutionVector):
             solutionVector[i1], solutionVector[i2] = solutionVector[i2], solutionVector[i1]
         if(calculateObjectiveFunction(solutionVector) <= objective_function):
             break
-    return solutionVector       
-    
-  
+    return solutionVector
+
+
 def proportional_probability(solutionVector):
     l = []
     for index, value in enumerate(solutionVector):
         if not value:
-            l.extend([index] * len(graphDictionary[index]))
+
+            temp1 = [index] * len(graphDictionary[index])
+            
+            l.extend(temp1)
+            
     return choice(l)
-	
+
+
 def inverse_probability(solutionVector):
     d = {}
     s = 0
@@ -45,22 +55,29 @@ def inverse_probability(solutionVector):
         if value:
             s += 1/len(graphDictionary[index])
             d[s] = index
-    p = uniform(0,s)
+    p = uniform(0, s)
     for i in d.keys():
         if p <= i:
             return d[i]
 
+
 def NodeReduction(solutionVector):
     solutionVector[inverse_probability(solutionVector)] = 0
-	return	
-def NodeAdd(SolutionVector):
+    return
+
+
+def NodeAdd(solutionVector):
     solutionVector[proportional_probability(solutionVector)] = 1
     return
+
+
 def NodeSwapper(solutionVector):
     i1 = int(choice(array(where(array(solutionVector) == 0)).T))
     i2 = int(choice(array(where(array(solutionVector) == 1)).T))
     solutionVector[i1], solutionVector[i2] = solutionVector[i2], solutionVector[i1]
-	return
+    return
+
+
 def getNodes(d):
     nodes = []
     for i in d.keys():
@@ -69,58 +86,62 @@ def getNodes(d):
         for j in d[i]:
             if j not in nodes:
                 nodes.append(j)
+
     return len(nodes)
+
 
 def MainLoop(initTemp, finalTemp, CoolingRatio, EpochLength, initSolutionVector):
     XBest = initSolutionVector
-    	y = initSolutionVector
-	Xk = initSolutionVector
-	
-	currentTemp=initTemp
-	while currentTemp > finalTemp : 
-		for M in range(1,EpochLength+1):
-            Temp1 = Xk
-			y=Temp1
-			if calculateObjectiveFunction(y) >= 1:
-                NodeReduction(y)
-                
+    y = initSolutionVector
+    Xk = initSolutionVector
 
-                deltaFunction = calculateObjectiveFunction(y) - calculateObjectiveFunction(Xk)
+    currentTemp = initTemp
+    while currentTemp > finalTemp:
+        for M in range(1, EpochLength+1):
+            Temp1 = Xk
+            y = Temp1
+            if calculateObjectiveFunction( 3, graphDictionary, y) >= 1:
+                NodeReduction(y)
+
+                deltaFunction = calculateObjectiveFunction( 3, graphDictionary, y) - calculateObjectiveFunction( 3, graphDictionary, Xk)
                 if deltaFunction > 0:
                     Temp1 = y
                     XBest = y
                     Xk = Temp1
                 else:
-                    probabilityAcceptance = 1/ (1 + math.exp(deltaFunction/currentTemp)
+                    probabilityAcceptance = 1 / (1 + math.exp(deltaFunction/currentTemp))
                     if random.random() < probabilityAcceptance:
-                        Xk = y
+                        Xk=y
+
                     else:
-                        Xk = Xk
-                
+                        Xk=Xk
             else:
                 NodeAdd(y)
-                if calculateObjectiveFunction(y) > calculateObjectiveFunction(Xk):
-                    Temp1 = y
-                    XBest = y
-                    Xk = Temp1
-            if calculateObjectiveFunction(y) < calculateObjectiveFunction(Xk):
-                Temp1 = Xk
-                z = Temp1
+                if calculateObjectiveFunction( 3, graphDictionary, y) > calculateObjectiveFunction( 3, graphDictionary, Xk):
+                    Temp1=y
+                    XBest=y
+                    Xk=Temp1
+            if calculateObjectiveFunction( 3, graphDictionary, y) < calculateObjectiveFunction( 3, graphDictionary, Xk):
+                Temp1=Xk
+                z=Temp1
                 NodeSwapper(z)
-                deltaFunction = calculateObjectiveFunction(z) - calculateObjectiveFunction(Xk)
+                deltaFunction=calculateObjectiveFunction( 3, graphDictionary, z) - calculateObjectiveFunction( 3, graphDictionary, Xk)
                 if deltaFunction > 0:
-                    Temp1 = z
-                    XBest = z
-                    Xk = Temp1
+                    Temp1=z
+                    XBest=z
+                    Xk=Temp1
                 else:
-                    probabilityAcceptance = 1/(1 + math.exp(deltaFunction/currentTemp))
+                    probabilityAcceptance=1 / (1 + math.exp(deltaFunction/currentTemp))
                     if random.random() < probabilityAcceptance:
-                        Xk = z
+                        Xk=z
                     else:
-                        Xk = Xk
-        
+                        Xk=Xk
+
         if currentTemp > finalTemp:
             currentTemp *= CoolingRatio
+            
+            
         else:
-            break        
-	return 
+            break
+    return XBest
+print(MainLoop(1500,150,0.95,10,[0,0,0,0,0,0,0,0,1]))
